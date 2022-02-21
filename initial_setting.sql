8e42149e6b26f0dab474447f98f2a601d89c5975
saihinchi-tak <86942851+saihinchi-tak@users.noreply.github.com>
2022-02-21 14:38:56 +0900

    Add files via upload

---
+++ initial_setting.sql
@@ -0,0 +1,141 @@
+USE flame_test;
+
+CREATE TABLE IF NOT EXSITS day0
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day1
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day2
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day3
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day4
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day5
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
+
+CREATE TABLE IF NOT EXSITS day6
+(
+  tweet_id BIGINT,
+  day_id DATETIME,
+  text VARCHAR(255),
+  created_at DATETIME,
+  user_friends MEDIUMINT,
+  user_followers MEDIUMINT,
+  retweet_count MEDIUMINT,
+  favorite_count MEDIUMINT,
+  AWS CHAR(10),
+  happy TINYINT,
+  sad TINYINT,
+  disgust TINYINT,
+  angry TINYINT,
+  surprise TINYINT,
+  day TINYINT,
+  risk TINYINT
+);
